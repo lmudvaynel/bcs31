78ae072aa5db0b3ac69f1fb16ee9005c343eadc2
Artyom Smirnov <smirnov@balticit.ru>
2013-07-19 11:08:37 +0300

    First rspec tests is ready

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Review do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:review) { FactoryGirl.build(:review) }
+
+  it 'should be valid with valid attributes' do
+    review.should be_valid
+  end
+
+  describe '#first_name' do
+    it 'should not be empty'
+  end
+
+  describe '#content' do
+    it 'should not be empty'
+  end
 end
