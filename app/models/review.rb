62b4e9d677cb71e449e21715080efb6ce1e141ad
Artyom Smirnov <smirnov@balticit.ru>
2013-07-19 11:53:31 +0300

    Tests for Review is green

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
   attr_accessible :city, :company_name, :content, :first_name, :last_name, :logo, :moderated
+
+  validates :first_name, :content, presence: true
 end
