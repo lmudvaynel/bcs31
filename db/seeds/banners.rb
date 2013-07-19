ee4e95961008eebe2ed7c742c9bc7a54aa0d2fbe
Nesit <nesitach@gmail.com>
2013-07-19 14:38:54 +0300

    Change banner seed

--- db/seeds/banners.rb
+++ db/seeds/banners.rb
@@ -1,7 +1,5 @@
 Banner.reset_column_information
 Banner.create!(
-  content: '<span class="paragraph_white">Оформи</span> <span class="paragraph_yellow">страховой</span>
-           <span class="paragraph_yellow">полис</span> <span class="paragraph_white">на выгодных</span>
-           <span class="paragraph_white">условиях</span>',
+  content: 'Гарантированная доставка точно в срок',
   url: '/'
 )
