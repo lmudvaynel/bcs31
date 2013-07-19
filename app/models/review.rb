4f4fa18c133db145e7f170fa5abe2bf4ae37bef1
Artyom Smirnov <smirnov@balticit.ru>
2013-07-19 11:58:28 +0300

    Mount uploader for Review logo

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ActiveRecord::Base
   attr_accessible :city, :company_name, :content, :first_name, :last_name, :logo, :moderated
 
+  mount_uploader :logo, ReviewLogoUploader
+
   validates :first_name, :content, presence: true
 end
---
+++ app/uploaders/review_logo_uploader.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+class ReviewLogoUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :thumb do
+    process :resize_to_fill => [200, 200]
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+end
