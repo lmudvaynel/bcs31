5521dbcee26f6ab484c07c526655e21c1d37cb1c
Nesit <nesitach@gmail.com>
2013-07-19 12:30:25 +0300

    Add banners

---
+++ app/models/banner.rb
@@ -0,0 +1,13 @@
+class Banner < ActiveRecord::Base
+  attr_accessible :content, :url
+
+  validates :content, presence: true
+
+  def paragraph(n)
+    content.split("\n").select(&:present?)[n]
+  end
+
+  def to_s
+    "Баннер ##{id}"
+  end
+end
