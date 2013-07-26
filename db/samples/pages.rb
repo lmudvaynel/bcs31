09dfaf08016a0179e818f6a338af60b67e38a7ec
Nesit <nesitach@gmail.com>
2013-07-26 12:18:01 +0300

    Add samples

--- db/samples/pages.rb
+++ db/samples/pages.rb
@@ -6,3 +6,17 @@ Page.reset_column_information
   page = FactoryGirl.create(:page, slug: slug)
   page.translations.find_by_locale(:ru).update_attribute :name, name
 end
+
+services = FactoryGirl.create(:page, slug: 'services')
+services.translations.find_by_locale(:ru).update_attribute :name, 'Услуги'
+
+{ order:    'Заказ (забор из другого города)',
+  express:  'Экспресс доставка',
+  assembled: 'Сборные грузы',
+  commander: 'Командирская почта',
+  tariffs: 'Тарифы' }.each do |slug, name|
+  page = FactoryGirl.create(:page, slug: slug)
+  page.parent = services
+  page.translations.find_by_locale(:ru).update_attribute :name, name
+  page.save!
+end
