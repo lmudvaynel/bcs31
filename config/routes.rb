cf10c55c6ab5327dfe75ea514472afe4c5931164
JonyLogy <jonylogy@gmail.com>
2013-08-02 11:24:50 +0300

    Some fix routes for online_courier

--- config/routes.rb
+++ config/routes.rb
@@ -11,18 +11,20 @@ Cite::Application.routes.draw do
 
   resources :feedbacks, :only => :create
 
+  resources :workers , :only  => :create
+  get 'about' => 'workers#index'
+
   resources :online_couriers, :only => :create
   get 'online_couriers' => 'online_couriers#new'
   get 'thanks' => 'online_couriers#thanks', :as => :thanks
 
-  resources :workers , :only  => :create
-  get 'about' => 'workers#index'
-
   resources :reviews, :only => [:index, :create]
   resources :news_pages
+
   get 'index' => 'pages#index'
   get 'news_pages/:id' => 'news_pages#show'
   get 'reviews' => 'reviews#index'
   get ':slug' => 'pages#show', :as => :slug
+
   resources :pages
 end
