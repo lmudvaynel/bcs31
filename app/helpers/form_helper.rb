e7a036e85951b4f75f1a052a4fd0e7abedb1b866
gaandrey <gribanovandrew@gmail.com>
2013-08-06 18:22:56 +0300

    Courier form style

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,9 +1,9 @@
 module FormHelper
   def online_courier_text_field(field, form)
     raw <<-HTML
-      <div class="field">
-        #{form.label(field, class: 'request_lable')}
-        #{form.text_field(field, class: 'input-block-level')}
+      <div class="courier_field_container">
+        #{form.label(field, class: 'field_name')}
+        #{form.text_field(field, class: 'text_field')}
       </div>
     HTML
   end
