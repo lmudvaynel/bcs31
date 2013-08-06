3527a07f52eec85e78694102beab63692f8ea7ce
Nesit <nesitach@gmail.com>
2013-08-06 12:43:40 +0300

    Rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Cite::Application.configure do
+Bcs31::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Cite::Application.config.session_store :cookie_store, key: '_cite_session'
+Bcs31::Application.config.session_store :cookie_store, key: '_bcs31_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Cite::Application.config.session_store :active_record_store
+# Bcs31::Application.config.session_store :active_record_store
