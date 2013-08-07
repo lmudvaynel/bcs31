5842b18099a06bc3037f418393ac622d0fc96b76
gaandrey <gribanovandrew@gmail.com>
2013-08-07 17:38:00 +0300

    I've got a chatbox

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Cite::Application.configure do
+Bcs31::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
