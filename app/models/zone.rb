c327f8da054f0b7ca4a95c8ca41281eaa6d4cee3
novoselov <novsem@gmail.com>
2013-08-07 15:18:02 +0300

    calc: add seed for bcs zones

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,13 +1,15 @@
 class Zone < ActiveRecord::Base
   extend Enumerize
 
-  attr_accessible :name, :provider
+  attr_accessible :name, :provider, :tariffs_attributes
 
   enumerize :provider, in: [:bcs, :major_express], predicates: true, scope: true
 
   has_many :zone_delivery_relations
   has_many :delivery_city_relations, through: :zone_delivery_relations
   has_many :tariffs
+
+  accepts_nested_attributes_for :tariffs
 
   validates :name, presence: true, uniqueness: { scope: :provider }
 
