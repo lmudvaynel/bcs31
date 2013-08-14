d2170396172cd04d5f297596cb39198781124996
Nesit <nesitach@gmail.com>
2013-08-14 14:38:31 +0300

    add email

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,11 @@
 class Review < ActiveRecord::Base
-  attr_accessible :city, :company_name, :content, :first_name, :last_name, :logo, :moderated
+  attr_accessible :city, :company_name, :content, :first_name, :last_name, :logo, :moderated, :email
 
   mount_uploader :logo, ReviewLogoUploader
 
   validates :first_name, :content, presence: true
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
   scope :moderated, -> { where(moderated: true) }
 
