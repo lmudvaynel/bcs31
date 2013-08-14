4e5e2edee39132001cf872cec13342ff67e6153b
Nesit <nesitach@gmail.com>
2013-08-14 11:52:41 +0300

    Add images to banners

--- spec/factories/banners.rb
+++ spec/factories/banners.rb
@@ -2,5 +2,8 @@ FactoryGirl.define do
   factory :banner do
     content Faker::HipsterIpsum.words(3).join("\n")
     url '/'
+    image   Rack::Test::UploadedFile.new(
+              Dir.glob(File.join(Rails.root, 'app', 'assets', 'images', 'slider_rocket.png')).sample
+            ) 
   end
 end
