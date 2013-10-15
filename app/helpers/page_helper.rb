45bd5dfe6cb1807177f322cb6d50256de9a3621e
Nastya <nastya@Lemon.(none)>
2013-10-15 14:40:54 +0300

    fix

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -6,5 +6,19 @@ module PageHelper
       'current'
     end
   end
-
+  
+  def title(page)
+    html_title = ''
+    if page.children.empty?
+       page = page.parent
+    end
+    html_title = (link_to page.name, slug_path(page))
+    while page.parent
+      page = page.parent
+      html_title = (link_to page.name, slug_path(page)) + ' < ' + html_title
+    end
+    raw <<-HTML
+          #{html_title}
+    HTML
+  end
 end
