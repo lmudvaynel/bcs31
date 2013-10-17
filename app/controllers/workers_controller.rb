2420338c0440ec49b48ab774fc33a8e8fe652820
Nesit <nesitach@gmail.com>
2013-10-17 19:02:23 +0300

    Fix js and staff order

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,6 +1,6 @@
 class WorkersController < ApplicationController
   def index
-    @workers = Worker.order("position DESC")
+    @workers = Worker.order("position ASC")
     @page = Page.find_by_slug("about")
   end
 end
