abec6d415c199af6b7e604835eb1df9ff1b5416b
gaandrey <gribanovandrew@gmail.com>
2013-11-05 15:30:38 +0300

    popups

--- app/assets/javascripts/iframe.js
+++ app/assets/javascripts/iframe.js
@@ -1,5 +1,6 @@
 $(window).load(function() {
     var v_port;
+    alert('ready');
      //////////////////  callus ifra,e
     $(".button_call_us.redish, .call_us_span").on('click', function(){
         $('.iframe_container_callus').css("display" , "block");
@@ -15,6 +16,7 @@ $(window).load(function() {
      ////////////////// indexes iframe
     $("a.service_link.indexes").on('click', function(){
         v_port = $(window).height();
+        console.log("hit");
         $('.indexes_iframe_container').css({'height': v_port -100 + "px", 'display': 'block'});
         $('#indexes_iframe').css({'height': v_port + 130 + "px", 'display': 'block'});
 //        $('#indexes_iframe').css("display" , "block");
@@ -30,8 +32,9 @@ $(window).load(function() {
     ////////////////// tracking iframe
     $("a.service_link.tracking").on('click', function(){
         v_port = $(window).height();
+        console.log("hit");
         $('.tracking_iframe_container').css({'height': v_port -150 + "px", 'display': 'block'});
-        $('#tracking_iframe').css({'height': v_port + "px", 'display': 'block'});
+        $('#tracking_iframe').css({'height': v_port + 500 + "px", 'display': 'block'});
 //        $('#indexes_iframe').css("display" , "block");
         $('.closing_cross').css("display" , "block");
 //        console.log(v_port);
@@ -43,23 +46,6 @@ $(window).load(function() {
         $('.tracking_iframe_container').css("display" , "none");
     });
 
-    $.fn.followTo = function (pos) {
-        var $this = this,
-            $window = $(window);
 
-        $window.scroll(function (e) {
-            if ($window.scrollTop() > pos) {
-                $this.css({
-                    position: 'absolute',
-                    top: pos
-                });
-            } else {
-                $this.css({
-                    position: 'fixed',
-                    top: 0
-                });
-            }
-        });
-    };
 
 });
