3be30ee39af42c70319db8a1b4d0eee4c9c93b53
Demko <artdemko@gmail.com>
2015-10-29 22:05:04 +0200

    Feedback

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,9 +1,11 @@
+# encoding: utf-8
 class FeedbackMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def feedback_email(recepient, feedback)
-    mail(to: recepient, subject: 'Новый отзыв', from: feedback.email) do |format|
+    mail(to: recepient, subject: 'У вас новый отзыв!', from: feedback.email) do |format|
       format.html { render text: feedback.message }
     end
   end
 end
+
