a4257af30754f625dec8e7d089bca439a2a697ed
Demko <artdemko@gmail.com>
2015-10-29 21:48:10 +0200

    Feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -4,10 +4,10 @@ class FeedbacksController < ApplicationController
     if request.xhr?
       @feedback = Feedback.new(params[:feedback])
       if @feedback.save
-        #AdminUser.all.each do |user|
-          #FeedbackMailer.feedback_email(user.email, @feedback).deliver
-        #end
         render json: {}, status: :ok
+        AdminUser.all.each do |user|
+          FeedbackMailer.feedback_email(user.email, @feedback).deliver
+        end
       else
         render json: { errors: @feedback.errors.messages }, status: :precondition_failed
       end
--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -2,7 +2,7 @@ class FeedbackMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def feedback_email(recepient, feedback)
-    mail(to: recepient, subject: 'You have new feedback message', from: feedback.email) do |format|
+    mail(to: recepient, subject: 'Новый отзыв', from: feedback.email) do |format|
       format.html { render text: feedback.message }
     end
   end
