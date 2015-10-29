ed8036cd5127093c682b82ee7a74c2144ea0c985
Demko <artdemko@gmail.com>
2015-10-29 21:26:42 +0200

    feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -4,9 +4,9 @@ class FeedbacksController < ApplicationController
     if request.xhr?
       @feedback = Feedback.new(params[:feedback])
       if @feedback.save
-        AdminUser.all.each do |user|
-          FeedbackMailer.feedback_email(user.email, @feedback).deliver
-        end
+        #AdminUser.all.each do |user|
+          #FeedbackMailer.feedback_email(user.email, @feedback).deliver
+        #end
         render json: {}, status: :ok
       else
         render json: { errors: @feedback.errors.messages }, status: :precondition_failed
