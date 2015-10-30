98e411c446afd01c1888cd643795be6c8ec8081b
Demko <artdemko@gmail.com>
2015-10-30 14:11:42 +0200

    mailer default

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -2,7 +2,7 @@ class ReviewMailer < ActionMailer::Base
   default from: "noreply@me31.ru"
 
   def review_email(recepient, review)
-    mail(to: recepient, subject: 'You have new review', from: "from@example.com") do |format|
+    mail(to: recepient, subject: 'You have new review') do |format|
       format.html { render text: review.content }
     end
   end
